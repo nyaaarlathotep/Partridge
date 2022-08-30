8862d639a19e449ff39ef92fd2e0f59067c9f135
yuegenhua <nyaaarlathotep@163.com>
2022-08-30 18:14:06 +0800

    feat sql

--- sql/partridge.sql
+++ sql/partridge.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : mysql_master
+ Source Server         : wsl2 docker mysql8.0
  Source Server Type    : MySQL
  Source Server Version : 80027
  Source Host           : 127.0.0.1:3306
@@ -11,7 +11,7 @@
  Target Server Version : 80027
  File Encoding         : 65001
 
- Date: 21/08/2022 22:05:48
+ Date: 30/08/2022 10:34:31
 */
 
 SET NAMES utf8mb4;
@@ -21,113 +21,124 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for actor
 -- ----------------------------
 DROP TABLE IF EXISTS `actor`;
-CREATE TABLE `actor` (
+CREATE TABLE `actor`  (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `NAME` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `CREATED_TIME` datetime NULL DEFAULT NULL,
+  `UPDATED_TIME` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for author
 -- ----------------------------
 DROP TABLE IF EXISTS `author`;
-CREATE TABLE `author` (
+CREATE TABLE `author`  (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `NAME` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `CREATED_TIME` datetime NULL DEFAULT NULL,
+  `UPDATED_TIME` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for ele_actor_re
 -- ----------------------------
 DROP TABLE IF EXISTS `ele_actor_re`;
-CREATE TABLE `ele_actor_re` (
+CREATE TABLE `ele_actor_re`  (
   `ID` int NOT NULL AUTO_INCREMENT,
   `ELE_ID` int NOT NULL,
   `ACTOR_ID` int NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for ele_author_re
 -- ----------------------------
 DROP TABLE IF EXISTS `ele_author_re`;
-CREATE TABLE `ele_author_re` (
+CREATE TABLE `ele_author_re`  (
   `ID` int NOT NULL AUTO_INCREMENT,
   `ELE_ID` int NOT NULL,
   `AUTHOR_ID` int NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for ele_org_re
 -- ----------------------------
 DROP TABLE IF EXISTS `ele_org_re`;
-CREATE TABLE `ele_org_re` (
+CREATE TABLE `ele_org_re`  (
   `ID` int NOT NULL AUTO_INCREMENT,
   `ELE_ID` int NOT NULL,
   `ORG_ID` int NOT NULL,
-  `RE_TYPE` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `RE_TYPE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for ele_tag_re
 -- ----------------------------
 DROP TABLE IF EXISTS `ele_tag_re`;
-CREATE TABLE `ele_tag_re` (
+CREATE TABLE `ele_tag_re`  (
   `ID` int NOT NULL AUTO_INCREMENT,
   `ELE_ID` int NOT NULL,
   `TAG_ID` int NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for element
 -- ----------------------------
 DROP TABLE IF EXISTS `element`;
-CREATE TABLE `element` (
+CREATE TABLE `element`  (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `TYPE` varchar(64) NOT NULL,
-  `RECORDED_TIME` datetime DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `TYPE` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `CREATED_TIME` datetime NULL DEFAULT NULL,
+  `UPDATED_TIME` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for jav
 -- ----------------------------
 DROP TABLE IF EXISTS `jav`;
-CREATE TABLE `jav` (
+CREATE TABLE `jav`  (
   `ELE_ID` int NOT NULL,
-  `CODE` varchar(64) NOT NULL COMMENT '識別碼',
-  `TITLE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `PUBLISH_DATE` datetime DEFAULT NULL COMMENT '發行日期',
-  `LENGTH` varchar(64) DEFAULT NULL COMMENT '長度',
-  `DIRECTOR` varchar(255) DEFAULT NULL,
-  `SERIES` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ELE_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `CODE` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '識別碼',
+  `TITLE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `PUBLISH_DATE` datetime NULL DEFAULT NULL COMMENT '發行日期',
+  `LENGTH` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '長度',
+  `DIRECTOR` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `SERIES` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `CREATED_TIME` datetime NULL DEFAULT NULL,
+  `UPDATED_TIME` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`ELE_ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for organization
 -- ----------------------------
 DROP TABLE IF EXISTS `organization`;
-CREATE TABLE `organization` (
+CREATE TABLE `organization`  (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `NAME` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `CREATED_TIME` datetime NULL DEFAULT NULL,
+  `UPDATED_TIME` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for tag
 -- ----------------------------
 DROP TABLE IF EXISTS `tag`;
-CREATE TABLE `tag` (
+CREATE TABLE `tag`  (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `NAME` varchar(255) NOT NULL,
-  `BIG_TYPE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `SOURCE` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=418 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `BIG_TYPE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `SOURCE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `CREATED_TIME` datetime NULL DEFAULT NULL,
+  `UPDATED_TIME` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
