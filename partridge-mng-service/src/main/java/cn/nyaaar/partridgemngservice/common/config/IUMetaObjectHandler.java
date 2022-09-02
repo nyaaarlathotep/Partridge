7e036e3485ec0dd69433c813617bbe5c738d7b48
yuegenhua <nyaaarlathotep@163.com>
2022-09-02 11:08:26 +0800

    feat updatedTime and createdTime

---
+++ partridge-mng-service/src/main/java/cn/nyaaar/partridgemngservice/common/config/IUMetaObjectHandler.java
@@ -0,0 +1,30 @@
+package cn.nyaaar.partridgemngservice.common.config;
+
+import cn.hutool.core.date.DateUtil;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+/**
+ * 自定义 MetaObjectHandler 类
+ *
+ * @author yuegenhua
+ * @Version $Id: IUMetaObjectHandler.java, v 0.1 2022-02 11:06 yuegenhua Exp $$
+ */
+@Slf4j
+@Component
+public class IUMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createdTime", DateUtil.now(), metaObject);
+        this.setFieldValByName("updatedTime", DateUtil.now(), metaObject);
+
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updatedTime", DateUtil.now(), metaObject);
+    }
+}
